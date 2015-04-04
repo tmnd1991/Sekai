2b8fb85fa6995ba5d67f29d20658ced5939ba6cf
Antonio Murgia <tmnd91@gmail.com>
2015-04-04 15:39:45 +0200

    firs commit

--- src/main/scala/it/murgia/antonio/sekai/base/package.scala
+++ src/main/scala/it/murgia/antonio/sekai/base/package.scala
@@ -3,9 +3,4 @@ package it.murgia.antonio.sekai
 package object base {
   type Identifier = String
   val NoOne : Identifier = ":__:__:__*__*__*"
-  object CanReceiveResponse extends Enumeration {
-    type CanReceiveResponse = Value
-    val YES, NO, DONTCONSUME = Value
-  }
-
 }
